92c987733c8cd0532b79cb77301026b67bef5f93
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-08-19 10:37:37 +0300

    modified

--- lib/book.rb
+++ lib/book.rb
@@ -1,25 +1,10 @@
-class Book
-    
-    attr_reader :title, :author, :page_count, :genre
-
-    def initialize(title)
-        @title= title
-    end
-
-    def author= (author)
-        @author= author
-    end
-
-    def page_count= (page_count)
-        @page_count= page_count
-    end
-
-    def genre= (genre)
-        @genre= genre
-    end
-
-    def turn_page
-        puts "Flipping the page...wow, you read fast!"
+# class Book
+class Book  
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title) 
+        @title = title
     end 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"   
+    end
 end
-
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,29 +1,12 @@
-# Make your shoe class here!
-class Shoe
-    attr_reader :brand, :color, :size, :material, :condition
+# # Make your shoe class here!
 
-    def initialize(brand)
-        @brand = brand
-    end
-
-    def color=(color)
-        @color = color
-    end
-
-    def size=(size)
-        @size = size
-    end
-
-    def material=(material)
-        @material = material
-    end
-
-    def condition=(condition)
-        @condition = condition
-    end
-
-    def cobble
-        puts "Your shoe is as good as new!"
-        @condition = "new"
-    end
+class Shoe  
+    attr_accessor :brand, :color, :size, :material, :condition 
+    def initialize(brand) 
+        @brand = brand 
+    end 
+    def cobble 
+        @condition = "new" 
+        puts "Your shoe is as good as new!" 
+    end 
 end
